f1df70565c0f08cc0027af143eb7100c4c8731fe
h-alan <battlebearboss@gmail.com>
2022-11-18 16:22:51 -0500

    Changed bash script to run multiple files

--- python/tests/parallelization/time-parallel-tests.sh
+++ python/tests/parallelization/time-parallel-tests.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-    echo "Script needs 1 arguments: python file"
-    echo "Example: bash parallel_tests.sh a.py"
+    echo "Script needs 1 arguments: python file dir"
+    echo "Example: bash parallel_tests.sh ."
 else
     script_dir=$( cd $( dirname $0 ) && pwd )
     logs_dir="$script_dir/logs"
-    test_file="$script_dir/${1}"
 
     mkdir -p $logs_dir
 
     pip uninstall -y pytest-xdist
 
     echo "Running non-parallel..."
-    ( time pytest $test_file ) &> "$logs_dir/non-parallel"
+    ( time pytest $1 ) &> "$logs_dir/non-parallel"
     echo "Finished non-parallel"
 
     pip install pytest-xdist
 
     echo "Running parallel..."
-    ( time pytest -n auto $test_file ) &> "$logs_dir/parallel"
+    ( time pytest -n auto $1 ) &> "$logs_dir/parallel"
     echo "Finished parallel"
 
     echo "Logs in: $logs_dir"
