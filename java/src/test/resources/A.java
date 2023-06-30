751af2ea9ee57c2d3b33d3b18f7bba9305638862
Yu Liu <yuki.liu@utexas.edu>
2023-06-30 01:18:21 -0700

    Delete java/src/test directory

--- java/src/test/resources/A.java
+++
@@ -1,9 +0,0 @@
-import org.inlinetest.Here;
-
-public class A {
-    public void process(int a, int b) {
-        a = a + b;
-        new Here().given(a, 2).given(b, 1).checkEq(a, 3);
-        a = a + b;
-    }
-}
--- java/src/test/resources/E.java
+++
@@ -1,9 +0,0 @@
-public class E {
-    public void process(int a, int b) {
-        a = a + b;
-        new Here(3).given(a, 2).given(b, 1).checkEq(a, 3);
-        new Here(3).given(a, -2).given(b, -1).checkEq(a, -3);
-        a = a - b;
-        new Here(6).given(a, 2).given(b, 1).checkEq(a, 1);
-    } 
-}
