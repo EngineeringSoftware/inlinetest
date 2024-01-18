db8bdd2be93fa63af838adb627ffbde24c838c44
Yu Liu <yuki.liu@utexas.edu>
2024-01-17 22:46:44 -0600

    do not load xml if object is not primitive type

--- data/examples/java/run-only.sh
+++ data/examples/java/run-only.sh
@@ -6,7 +6,7 @@ inline_test_path=$1; shift
 example=$1; shift
 
 set -e
-java -jar $inline_test_path --output_mode=new --assertion_style=assert --input_file=${example}.java
+java -jar $inline_test_path --output_mode=new --assertion_style=assert --input_file=${example}.java --load_xml=false
 src=${example}Test.java
 if [[ -d ${example} ]]; then
         src="$src ${example}/*.java"
